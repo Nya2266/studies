dd6a6796b489586333cbfd1fed903827b0bf7a7d
Nyalith <eubadgeworld@gmail.com>
2020-11-23 20:59:04 +0100

    fun1 final

--- mejorceasercipher.py
+++ mejorceasercipher.py
@@ -1,28 +1,17 @@
-abc = ("abcdefghijklmnñopqrstuvwxyz")
-cipher = ""
-step = 0
-post_cipher = ""
+def mejor_ceasar():
+    text = input("Text to cipher")
+    step = int(input("GIVE ME THAT STEPA TO CIPHER"))
+    cipher = ""
+    for i in text:
+        if not i.isalpha():
+            continue
+        code = ord(i) + step
+        cipher += chr(code)
+        print(i)
+        print(code)
+    print("original text:", text)
+    print("Ciphered text:", cipher)
 
-def InitData():
-    global cipher, step
-    cipher = input("Message to cipher")
-    try:
-        step = int(input("Choose the step to cipher 1-25 steps"))
-    except ValueError: print("Please introduce an integer between 1 and 25"), InitData()
-    except: print("Unknown error please try again"), InitData()
-    if step > 0 and step < 26:
-        pass
-    else:
-        print("Incorrect data please try again")
-        InitData()
+mejor_ceasar()
 
-
-def cipher_now(cipher, step):
-    for i in cipher:
-        pos = abc.index(i)
-
-
-abclist = []
-print("Teste")
-
-#TRY TWO
+print(chr(110))
